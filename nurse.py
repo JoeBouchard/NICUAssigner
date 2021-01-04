40f5545d5177cd47a49a3cf0834c55ee82770d2d
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2021-01-04 01:21:07 -0600

    Created first stab at nurse class

---
+++ nurse.py
@@ -0,0 +1,16 @@
+class Nurse:
+    def __init__(self, name='foo', attrs=[]):
+        """name is a string of the Nurse's name
+        attrs is a list of the Nurse's Attributes"""
+        
+        self.name = name
+        self.attrs = attrs
+
+    def getName(self):
+        return self.name
+
+    def getAttrs(self):
+        return self.attrs
+
+    def addAttr(self, attr):
+        self.attrs.append(attrs)
