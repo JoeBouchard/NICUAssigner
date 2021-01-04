dfc153d95b7d7bcbe4953a60eae2553b0c6821dc
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2021-01-04 01:31:41 -0600

    First attempt at patient class

---
+++ patient.py
@@ -0,0 +1,16 @@
+class Patient:
+    def __init__(self, name='bar', attrs=[]):
+        """name is a string of the Patient's name
+        attrs is a list of the Patient's Attributes"""
+        
+        self.name = name
+        self.attrs = attrs
+
+    def getName(self):
+        return self.name
+
+    def getAttrs(self):
+        return self.attrs
+
+    def addAttr(self, attr):
+        self.attrs.append(attrs)
