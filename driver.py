146e0854760eb8fb8f2194f2f4c4ae6356924cef
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2021-01-04 01:32:25 -0600

    Updated pack with Labels and added import for patient

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from nurse import *
 from attributes import *
+from patient import *
 
 testing = True
 
@@ -11,7 +12,11 @@ class MainScreen:
         self.nurses = []
         self.attrs = []
         self.patients = []
-        self.nurseDisplay = Listbox(self.tk)
+        self.nurseFrame = Frame(self.tk)
+        self.nurseDisplay = Listbox(self.nurseFrame)
+
+        self.patientFrame = Frame(self.tk)
+        self.patientDisplay = Listbox(self.patientFrame)
         self.pack()
 
     def addNurse(self, name, attrs):
@@ -22,9 +27,16 @@ class MainScreen:
         for nNum in range(0, len(self.nurses)):
             self.nurseDisplay.insert(nNum, self.nurses[nNum].getName())
 
-
     def pack(self):
-        self.nurseDisplay.pack()
+        self.nurseLabel = Label(self.nurseFrame, text="Nurses:")
+        self.nurseLabel.pack(side='top')
+        self.nurseDisplay.pack(side='bottom')
+        self.nurseFrame.pack(side='left')
+
+        self.patientLabel = Label(self.patientFrame, text="Patients:")
+        self.patientLabel.pack(side='top')
+        self.patientDisplay.pack(side='bottom')
+        self.patientFrame.pack(side='right')
     
         
 m=MainScreen("Hello")
